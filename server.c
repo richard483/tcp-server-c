cc1a565fd0da6b3123cafb9222f8db663e1430f7
richard483 <richard.william483@gmail.com>
2026-03-20 05:01:52 +0700

    added sockopt reuseaddr

--- server.c
+++ server.c
@@ -5,25 +5,36 @@
 #include <string.h>
 
 #define LISTEN_BACKLOG 10
+#define SOCKET_OPT_VAL 1
 
 int main()
 {
-  int dock, my_bind, my_listen, my_accept;
+  struct sockaddr_in my_addr, client_addr;
+
+  int dock, my_bind, my_listen, my_accept, my_sockopt;
+  int socketoptval = SOCKET_OPT_VAL;
   char buffer[1024];
   char response[] = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
-  struct sockaddr_in my_addr, client_addr;
+
   socklen_t client_len;
   ssize_t read_result, my_write;
+
   memset(&my_addr, 0, sizeof(my_addr));
   my_addr.sin_family = AF_INET;
   my_addr.sin_port = htons(8080);
   my_addr.sin_addr.s_addr = INADDR_ANY;
 
   dock = socket(AF_INET, SOCK_STREAM, 0);
-
   if (dock == -1)
   {
     perror("Socket is failing\n");
+    return 1;
+  }
+
+  my_sockopt = setsockopt(dock, SOL_SOCKET, SO_REUSEADDR, &socketoptval, sizeof(socketoptval));
+  if (my_sockopt == -1)
+  {
+    perror("Sockopt is failing");
     return 1;
   }
 
@@ -62,7 +73,15 @@ int main()
     my_write = write(my_accept, response, strlen(response));
     close(my_accept);
 
-    printf("Heres the program result:\ndock: %d\nbind: %d\nlisten: %d\naccept: %d\nread: %ld\nbuffer: %s\nwrite: %ld\n", dock, my_bind, my_listen, my_accept, read_result, buffer, my_write);
+    printf("Heres the program result:\n\n");
+    printf("dock: %d\n", dock);
+    printf("sockopt: %d\n", my_sockopt);
+    printf("bind: %d\n", my_bind);
+    printf("listen: %d\n", my_listen);
+    printf("accept: %d\n", my_accept);
+    printf("read: %ld\n", read_result);
+    printf("buffer: %s\n", buffer);
+    printf("write: %ld\n", my_write);
   }
 
   return 0;
