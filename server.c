2ccc93f7ba5707e7b48705c6b6dc5b067cd53bbc
richard483 <richard.william483@gmail.com>
2026-03-20 03:29:49 +0700

    added looping & close on every write

--- server.c
+++ server.c
@@ -10,19 +10,14 @@ int main()
 {
   int dock, my_bind, my_listen, my_accept;
   char buffer[1024];
+  char response[] = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
   struct sockaddr_in my_addr, client_addr;
   socklen_t client_len;
-  ssize_t read_result;
-
+  ssize_t read_result, my_write;
   memset(&my_addr, 0, sizeof(my_addr));
   my_addr.sin_family = AF_INET;
   my_addr.sin_port = htons(8080);
   my_addr.sin_addr.s_addr = INADDR_ANY;
-
-  memset(&client_addr, 0, sizeof(client_addr));
-  client_len = sizeof(client_addr);
-
-  memset(&buffer, 0, sizeof(buffer));
 
   dock = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -48,15 +43,27 @@ int main()
     return 1;
   }
 
-  my_accept = accept(dock, (struct sockaddr *)&client_addr, &client_len);
-  if (my_accept == -1)
+  while (1)
   {
-    perror("Accept is failing");
-    return 1;
+    memset(&client_addr, 0, sizeof(client_addr));
+    client_len = sizeof(client_addr);
+
+    memset(&buffer, 0, sizeof(buffer));
+
+    my_accept = accept(dock, (struct sockaddr *)&client_addr, &client_len);
+    if (my_accept == -1)
+    {
+      perror("Accept is failing");
+      return 1;
+    }
+
+    read_result = read(my_accept, buffer, sizeof(buffer) - 1);
+
+    my_write = write(my_accept, response, strlen(response));
+    close(my_accept);
+
+    printf("Heres the program result:\ndock: %d\nbind: %d\nlisten: %d\naccept: %d\nread: %ld\nbuffer: %s\nwrite: %ld\n", dock, my_bind, my_listen, my_accept, read_result, buffer, my_write);
   }
 
-  read_result = read(my_accept, buffer, sizeof(buffer) - 1);
-
-  printf("Heres the program result:\ndock: %d\nbind: %d\nlisten: %d\naccept: %d\nread: %ld\nbuffer: %s", dock, my_bind, my_listen, my_accept, read_result, buffer);
   return 0;
 }
