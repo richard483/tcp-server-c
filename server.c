847c815405a675809da0728186c2094e0e7a9297
richard483 <richard.william483@gmail.com>
2026-03-20 03:01:44 +0700

    done accepted connection

--- server.c
+++ server.c
@@ -6,17 +6,14 @@
 
 #define LISTEN_BACKLOG 10
 
-int main() {
+int main()
+{
+  int dock, my_bind, my_listen, my_accept;
+  char buffer[1024];
   struct sockaddr_in my_addr, client_addr;
   socklen_t client_len;
+  ssize_t read_result;
 
-  int dock = socket(AF_INET, SOCK_STREAM, 0);
-
-  if (dock == -1) {
-    perror("Socket is failing\n");
-    return 1;
-  }
-  
   memset(&my_addr, 0, sizeof(my_addr));
   my_addr.sin_family = AF_INET;
   my_addr.sin_port = htons(8080);
@@ -25,26 +22,41 @@ int main() {
   memset(&client_addr, 0, sizeof(client_addr));
   client_len = sizeof(client_addr);
 
-  int my_bind = bind(dock, (struct sockaddr *) &my_addr, sizeof(my_addr));
+  memset(&buffer, 0, sizeof(buffer));
 
-  if (my_bind == -1) {
+  dock = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (dock == -1)
+  {
+    perror("Socket is failing\n");
+    return 1;
+  }
+
+  my_bind = bind(dock, (struct sockaddr *)&my_addr, sizeof(my_addr));
+
+  if (my_bind == -1)
+  {
     perror("Bind is failing");
     return 1;
   }
 
-  int my_listen = listen(dock, LISTEN_BACKLOG);
+  my_listen = listen(dock, LISTEN_BACKLOG);
 
-  if (my_listen == -1) {
+  if (my_listen == -1)
+  {
     perror("Listen is failing");
     return 1;
   }
 
-  int my_accept = accept(dock,  (struct sockaddr *) &client_addr, &client_len);
-  if (my_accept == -1) {
+  my_accept = accept(dock, (struct sockaddr *)&client_addr, &client_len);
+  if (my_accept == -1)
+  {
     perror("Accept is failing");
     return 1;
   }
 
-  printf("Heres the dock result: %d and the bind result: %d and the listen result: %d and the accept: %d\n", dock, my_bind, my_listen, my_accept);
+  read_result = read(my_accept, buffer, sizeof(buffer) - 1);
+
+  printf("Heres the program result:\ndock: %d\nbind: %d\nlisten: %d\naccept: %d\nread: %ld\nbuffer: %s", dock, my_bind, my_listen, my_accept, read_result, buffer);
   return 0;
 }
